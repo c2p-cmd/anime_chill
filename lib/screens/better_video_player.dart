833a2593ab276769d16fb237d5df0ac5066f37a3
Sharan Thakur <sharandt19@live.com>
2023-05-07 21:20:33 +0530

    migration to playout

--- lib/screens/better_video_player.dart
+++ lib/screens/better_video_player.dart
@@ -1,69 +1,82 @@
-import 'package:appinio_video_player/appinio_video_player.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_playout/player_state.dart';
+import 'package:flutter_playout/video.dart';
 
 class BetterVideoPlayer extends StatefulWidget {
   final String url;
+  final String? title;
   const BetterVideoPlayer({
     Key? key,
     required this.url,
+    this.title,
   }) : super(key: key);
 
   @override
   State<BetterVideoPlayer> createState() => _BetterVideoPlayerState();
 }
 
-class _BetterVideoPlayerState extends State<BetterVideoPlayer> {
-  late VideoPlayerController player;
-  late CustomVideoPlayerController customVideoPlayerController;
+class _BetterVideoPlayerState extends State<BetterVideoPlayer> with AutomaticKeepAliveClientMixin<BetterVideoPlayer> {
+  final _focusNode = FocusNode();
+  var _playerState = PlayerState.PAUSED;
 
   @override
   void initState() {
-    player = VideoPlayerController.network(widget.url)
-      ..setVolume(1.0)
-      ..initialize().then((value) {
-        if (mounted) {
-          setState(() {});
-        }
-      });
-    customVideoPlayerController = CustomVideoPlayerController(
-      context: context,
-      videoPlayerController: player,
-      customVideoPlayerSettings: const CustomVideoPlayerSettings(
-        placeholderWidget: CupertinoActivityIndicator(),
-        deviceOrientationsAfterFullscreen: [
-          DeviceOrientation.portraitUp,
-        ],
-        alwaysShowThumbnailOnVideoPaused: true,
-        customVideoPlayerProgressBarSettings:
-            CustomVideoPlayerProgressBarSettings(
-          progressColor: Colors.deepPurpleAccent,
-          bufferedColor: Color(0x5A7C4DFF),
-        ),
-      ),
-    );
     super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
   }
 
   @override
   void dispose() {
-    customVideoPlayerController.dispose();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
     return Scaffold(
-      appBar: AppBar(
-        leading: const BackButton(),
-        backgroundColor: Colors.deepPurpleAccent,
+      appBar: const CupertinoNavigationBar(
+        leading: BackButton(),
+        backgroundColor: Colors.transparent,
       ),
       body: SafeArea(
-        child: CustomVideoPlayer(
-          customVideoPlayerController: customVideoPlayerController,
+        child: KeyboardListener(
+          focusNode: _focusNode,
+          autofocus: true,
+          onKeyEvent: (keyEvent) async {
+            if (keyEvent is KeyUpEvent) {
+              if (keyEvent.logicalKey == LogicalKeyboardKey.space) {
+                if (_playerState == PlayerState.PAUSED) {
+                  setState(() {
+                    _playerState = PlayerState.PLAYING;
+                  });
+                }
+                if (_playerState == PlayerState.PLAYING) {
+                  setState(() {
+                    _playerState = PlayerState.PAUSED;
+                  });
+                }
+              }
+            }
+          },
+          child: Video(
+            url: widget.url,
+            title: widget.title,
+
+            desiredState: _playerState,
+          ),
         ),
       ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
