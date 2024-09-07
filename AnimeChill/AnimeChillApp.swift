2d11d57d5fa3e5f1e9bfb8f14c9513693bee6805
Sharan Thakur <sharandt19@live.com>
2024-09-07 13:10:43 +0530

    Initial Commit

---
+++ AnimeChill/AnimeChillApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeChillApp.swift
+//  AnimeChill
+//
+//  Created by Sharan Thakur on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimeChillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
